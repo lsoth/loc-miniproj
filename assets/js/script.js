d3f4a953a5f65ec26c81e9f8ebff574bb28298ff
Koppi Kolyvek <kk674@cornell.edu>
2021-07-02 13:26:10 -0700

    fixed api url builder formatting

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,11 +7,12 @@
 
 // EVENT LISTENERS FOR SEARCH BAR AND SEARCH FORMAT DROPDOWN
 
-$('#SEARCHBUTTON').on('click', function () {
-    // build api query from search term and format inputs
-    var searchTerm = $('#SEARCHBAR').val();
-    var searchFormat = $('#SEARCHFORMAT').val();
+$('#search').on('click', function(event) {
+    event.preventDefault();
 
+    // build api query from search term and format inputs
+    var searchTerm = $('#searchTerm').val();
+    var searchFormat = $('#searchFormat').val();
 
     if (!searchTerm) {
         alert("Please enter a search term!");
@@ -21,10 +22,16 @@ $('#SEARCHBUTTON').on('click', function () {
     var apiBase = 'https://www.loc.gov';
     
     if (!searchFormat) {
-        var apiRequest = apiBase + '/search/?q=' + searchTerm.replace(' ', '-') + '&fo=json';
-        console.log(apiRequest);
+        var apiRequest = apiBase + '/search/?q=' + searchTerm + '&fo=json';
+
+        fetch(apiRequest)
+        .then(response => response.json())
+        .then(function (data) {
+            console.log(data.results[0].description);
+        });
+
     } else {
-        var apiRequest = apiBase + '/' + searchFormat.replace(' ', '-') + '/?q=' + searchTerm.replace(' ', '-') + '&fo=json';
+        var apiRequest = apiBase + '/' + searchFormat.replace(' ', '-') + '/?q=' + searchTerm + '&fo=json';
         console.log(apiRequest);
-    }
+    };
 });
